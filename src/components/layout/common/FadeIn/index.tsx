1d795c9fa71e484d5d273abd4a0fed4b67e14d3b
markYoon <markup3604@gmail.com>
2023-01-09 13:24:11 +0900

    add : emotion prgama 전역설정

---
+++ src/components/layout/common/FadeIn/index.tsx
@@ -0,0 +1,11 @@
+import { css, jsx } from "@emotion/react";
+
+const IndexBlock = css`
+  color: red;
+`;
+
+const index = () => {
+  return <div css={IndexBlock}></div>;
+};
+
+export default index;
